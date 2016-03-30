1db47058b4b3c9f016b95997b17e94700ec8c823
PenneyGadget <dreamofgreen@gmail.com>
2016-03-30 14:04:08 -0600

    First test passing

---
+++ test/game-test.js
@@ -0,0 +1,12 @@
+var assert = require('chai').assert;
+const Game = require('../lib/scripts/game');
+
+it('Game is a function', function() {
+  assert.typeOf(Game, 'function');
+});
+
+describe('stuff', function() {
+  xit('does stuff', function() {
+
+  });
+});
